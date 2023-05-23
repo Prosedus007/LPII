dc3404b8fc2b27a8f5837f3b2938840ba0e1a036
Prosedus007 <100461981+Prosedus007@users.noreply.github.com>
2023-05-23 22:06:02 +0530

    first commit

--- wad/w3/main.js
+++ wad/w3/main.js
@@ -1,58 +1,69 @@
-function FetchData()
-{
-    var xhr = new XMLHttpRequest()
-    xhr.open("GET","https://jsonplaceholder.typicode.com/users")
-    xhr.send()
-    xhr.onload=()=>{
-        let response = xhr.responseText
-        localStorage.setItem("users",response)
-    }
-    DisplayData()
-}
-
-function DisplayData()
-{
-    let tbody = document.getElementById("tbody")
-    let users=JSON.parse(localStorage.getItem("users"))
-    tbody.innerHTML=""
+function DisplayData() {
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let html = ` <center>
+    <table border='2px'>
+        <thead>
+            <tr>
+                <th>
+                    Name
+                </th>
+                <th>
+                    Phone-Number
+                </th>
+                <th>
+                    Division
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+    `;
     users.forEach(element => {
-        tbody.innerHTML+=
-    `
-        <tr>
-            <td>${element.name}</td>
-            <td>${element.username}</td>
-            <td>${element.email}</td>
-        </tr>
-    `
-    });
+        html += `
+            <tr>
+                <td>${element?.name}</td>
+                <td>${element?.phone}</td>
+                <td>${element?.div || "11"}</td>
+            </tr>
+        `
+    })
+
+    html += '</tbody> </table></center>';
+
+    const w = open()
+    w.document.body.innerHTML = html;
 }
 
-FetchData();
+function addAndDisplayData(data) {
+    let arr = JSON.parse(localStorage.getItem('users')) || []; // if array not present in localStorage then take empty array
+    arr.unshift(data); // add element at the start of array
+    localStorage.setItem('users', JSON.stringify(arr));
+    DisplayData();
+}
 
+document.forms.registrationForm.addEventListener("submit", formSubmit)
 
-document.getElementById("btn").addEventListener('click',()=>{
-    let name = document.getElementById('name').value
-    let username = document.getElementById('phone_no').value
-    let email = document.getElementById('Div').value
+function formSubmit(event) {
+    event.preventDefault();
 
-    let postObj = {
-        name,username,email
-    }
+    let name = document.getElementById('name').value;
+    let phone = document.getElementById('phone').value;
+    let div = document.getElementById('div').value;
 
-    let xhr = new XMLHttpRequest()
-    xhr.open("POST","https://jsonplaceholder.typicode.com/users")
-    xhr.setRequestHeader('Content-type','application/json','charset=UTF-8')
-    xhr.send(JSON.stringify(postObj))
+    let postObj = { name, phone, div };
 
-    xhr.onload=()=>{
-        if(xhr.status==201)
-        {
-            let arr=JSON.parse(localStorage.getItem('users'))
-            arr.unshift(postObj)
-            localStorage.setItem('users',JSON.stringify(arr))
-            DisplayData()
+    $.ajax({
+        type: 'POST',
+        url: 'https://jsonplaceholder.typicode.com/users',
+        data: JSON.stringify(postObj),
+        contentType: "application/json",
+
+        success: function (newUser) {
+            addAndDisplayData(postObj)
+        },
+        error: function (error) {
+            console.log(error)
+            addAndDisplayData(postObj)
         }
-    }
+    });
 
-})
-
+}
